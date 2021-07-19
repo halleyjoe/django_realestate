e45adecbc210ef9cd579b8f91359ceac7cf6c5db
unknown <josephwillies@gmail.com>
2021-07-19 13:29:19 +0530

    commented send file in contacts.views.py

--- contacts/views.py
+++ contacts/views.py
@@ -26,13 +26,13 @@ def contact(request):
 
         contact = Contact(listing=listing, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
-        send_mail(
-            'Property listing Inquiry'
-            'There has been an inquiry for ' + listing + '. Singn into the admin panel for more info',
-            'josephwillies@gmail.com',
-            [realtor_email, 'rambojoewillis@gmail.com'],
-            fail_silently=False
-        )
+        # send_mail(
+        #     'Property listing Inquiry'
+        #     'There has been an inquiry for ' + listing + '. Singn into the admin panel for more info',
+        #     'josephwillies@gmail.com',
+        #     [realtor_email, 'rambojoewillis@gmail.com'],
+        #     fail_silently=False
+        # )
         messages.success(request,'Your request has been submitted, a realtor will get back to you soon')
         return redirect('/listings/'+listing_id)
     
